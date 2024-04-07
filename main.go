aa31529d716e488fcd30b081ebc0557d298178e6
ebrardev <16155052@mersin.edu.tr>
2024-04-08 02:50:02 +0300

    Add GET /events endpoint to main.go

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	// server.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "hello server",
+	})
+
 }
