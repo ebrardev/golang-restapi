010a0a5ef28b24836f6ab224e0cf84d6d59b17fd
ebrardev <16155052@mersin.edu.tr>
2024-04-20 13:38:47 +0300

    refactoring

---
+++ routes/events.go
@@ -0,0 +1,55 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"ebrarcode.dev/restapi-go/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "error while fetching events"})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+
+}
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "id should be integer"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "error while fetching event"})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "  Could not create  event."})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"status": "event created", "event": event})
+}
