c9d7024f7d626fff8e21ff9daa451da84be74660
ebrardev <16155052@mersin.edu.tr>
2024-04-21 09:37:26 +0300

    delete event

--- routes/events.go
+++ routes/events.go
@@ -82,3 +82,22 @@ func updateEvent(context *gin.Context) {
 	}
 	context.JSON(http.StatusOK, gin.H{"status": "event updated", "event": updatedEvent})
 }
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "id should be integer"})
+		return
+	}
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": "event not found"})
+		return
+	}
+	err = event.Delete()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete event"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"status": "event deleted"})
+
+}
